5af610369f5f7f5bd13d352bae83f856fc52b32e
Lithuinia <netomas3@gmail.com>
2025-10-06 15:44:19 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,42 @@
 import { world, BlockPermutation } from "@minecraft/server";
 
-world.afterEvents.worldLoad.subscribe(() => {
-    world.afterEvents.playerBreakBlock.subscribe((event) => {
-        const brokenType = event.brokenBlockPermutation.type.id; // the one who was demolished
-        let isPlant = false;
-        let plant;
-        let grothStateCount = 7;
-        switch (brokenType) {
-            case ("minecraft:wheat"):
-                plant = "minecraft:wheat";
-                isPlant = true;
-                break;
-            case ("minecraft:carrots"):
-                plant = "minecraft:carrots";
-                isPlant = true;
-                break;
-            case ("minecraft:potatoes"):
-                plant = "minecraft:potatoes";
-                isPlant = true;
-                break;
-            case ("minecraft:beetroot"):
-                plant = "minecraft:beetroot";
-                isPlant = true;
-                break;
+
+world.afterEvents.playerBreakBlock.subscribe((event) => {
+    const brokenType = event.brokenBlockPermutation.type.id; // the one who was demolished
+    let isPlant = false;
+    let plant;
+    let grothStateCount = 7;
+    switch (brokenType) {
+        case ("minecraft:wheat"):
+            plant = brokenType;
+            isPlant = true;
+            break;
+        case ("minecraft:carrots"):
+            plant = brokenType;
+            isPlant = true;
+            break;
+        case ("minecraft:potatoes"):
+            plant = brokenType;
+            isPlant = true;
+            break;
+        case ("minecraft:beetroot"):
+            plant = brokenType;
+            isPlant = true;
+            break;
+    }
+
+
+    if (isPlant) {
+        const growthState = event.brokenBlockPermutation.getState("growth");
+        const leSneak = event.player.isSneaking;
+
+        if (growthState === grothStateCount && leSneak) {
+
+            const location = event.block.location;
+            const dimension = event.dimension;
+            const leBlock = BlockPermutation.resolve(plant, { "growth": 0 });
+            dimension.setBlockPermutation(location, leBlock);
+
         }
-
-
-        if (isPlant) {
-            const growthState = event.brokenBlockPermutation.getState("growth");
-            const leSneak = event.player.isSneaking;
-
-            if (growthState === grothStateCount && leSneak) {
-
-                const location = event.block.location;
-                const dimension = event.dimension;
-                const leBlock = BlockPermutation.resolve(plant, { "growth": 0 });
-                dimension.setBlockPermutation(location, leBlock);
-
-            }
-        }
-    });
+    }
 });
-
-
